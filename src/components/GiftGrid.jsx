e830629cd418c5ccbb938baec6e4739866c4f174
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-08-13 22:36:24 -0500

    xreated new helpers getGifs.

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { getGifs } from '../helpers/getGifs';
 
 const GiftGrid = ({ category }) => {
 
-  return (
+    getGifs();
+
+    return (
     <>
-        <h3>{category}</h3>   
+        <h3>{category}</h3>  
     </>
-  )
+    )
 }
 
 export default GiftGrid
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=QWggCM1TOk6DJiwY3SNgZt4ToU2kpe1j&limit=10`;   
+    const response = await fetch(url);
+    const { data } = await response.json();
+    
+    const gifs = data.map(img =>({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    return gifs;
+}
