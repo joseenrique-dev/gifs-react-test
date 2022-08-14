e0fe92204f1e7f7f36b7deb1974d73fba72c663e
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-08-13 23:43:32 -0500

    created component for gif item.

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs';
+import GifItem from './GifItem';
 
 const GiftGrid = ({ category }) => {
+    const [images, setImages] = useState([]);
 
-    getGifs();
+    const getImages = async () => {
+        const images = await getGifs(category);
+        console.log('IMG',images);
+        setImages(images);
+    }
+
+    useEffect(() => {
+        getImages()
+    } , []);
 
     return (
     <>
-        <h3>{category}</h3>  
+        <h3>{category}</h3>
+        <div className='card-grid'>
+            {
+                images.map( img => 
+                    <GifItem
+                        key={img.id} 
+                        { ...img }
+                    />
+                    )
+            }
+        </div>
     </>
     )
 }
