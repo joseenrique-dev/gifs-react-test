0c61bb8f22bcdef13e4d3694644d25bc86c981f3
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-08-17 01:20:45 -0500

    added more test to GifItem

--- tests/components/GiftItems.test.jsx
+++ tests/components/GiftItems.test.jsx
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('GifItems tests', () => {
     const fakeTitle = 'Fake title';
-    const fakeUrl = 'Fake url';
+    const fakeUrl = 'http://fakeurl.com/';
     it('should render correctly', () => {
-        const {container} = render(<GifItem title={fakeTitle } url={fakeUrl} />);
+        const {container} = render(<GifItem title={ fakeTitle } url={ fakeUrl } />);
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should render the image with the correct url and the indicated ALT', () => {
+        render(<GifItem title={ fakeTitle } url={ fakeUrl } />);
+        expect(screen.getByRole('img').src).toBe(fakeUrl);
+        expect(screen.getByRole('img').alt).toBe(fakeTitle);
     });
 });
